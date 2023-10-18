0c5cf387dbb2c4882c985c565892b659a19d1f06
AlexanderDerevliov <alex.feelred@gmail.com>
2023-10-18 13:40:22 +0200

    Implemented ReadMe and first step on js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+// step 1
+
+const numKm = Number (prompt (`Inserire la distanza che si vuole percorrere`));
+console.log (numKm, typeof numKm);
+
+document.getElementById (`km`) .innerHTML = `La distanza selezionata è: ${numKm} km`;
+
+
+// step 2
